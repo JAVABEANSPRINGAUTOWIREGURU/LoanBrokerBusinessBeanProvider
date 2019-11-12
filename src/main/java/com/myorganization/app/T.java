a68952d70825e331c1b4c80ea284dffbb9b943f2
JAVASPRING AUTOWIRE GURU <57644059+JAVABEANSPRINGAUTOWIREGURU@users.noreply.github.com>
2019-11-12 00:22:18 +0000

    Create T.java

---
+++ src/main/java/com/myorganization/app/T.java
@@ -0,0 +1,104 @@
+public class T
+{
+    private String C_NATIONKEY;
+
+    private String C_ADDRESS;
+
+    private String C_NAME;
+
+    private String C_PHONE;
+
+    private String C_ACCTBAL;
+
+    private String C_COMMENT;
+
+    private String C_MKTSEGMENT;
+
+    private String C_CUSTKEY;
+
+    public String getC_NATIONKEY ()
+    {
+        return C_NATIONKEY;
+    }
+
+    public void setC_NATIONKEY (String C_NATIONKEY)
+    {
+        this.C_NATIONKEY = C_NATIONKEY;
+    }
+
+    public String getC_ADDRESS ()
+    {
+        return C_ADDRESS;
+    }
+
+    public void setC_ADDRESS (String C_ADDRESS)
+    {
+        this.C_ADDRESS = C_ADDRESS;
+    }
+
+    public String getC_NAME ()
+    {
+        return C_NAME;
+    }
+
+    public void setC_NAME (String C_NAME)
+    {
+        this.C_NAME = C_NAME;
+    }
+
+    public String getC_PHONE ()
+    {
+        return C_PHONE;
+    }
+
+    public void setC_PHONE (String C_PHONE)
+    {
+        this.C_PHONE = C_PHONE;
+    }
+
+    public String getC_ACCTBAL ()
+    {
+        return C_ACCTBAL;
+    }
+
+    public void setC_ACCTBAL (String C_ACCTBAL)
+    {
+        this.C_ACCTBAL = C_ACCTBAL;
+    }
+
+    public String getC_COMMENT ()
+    {
+        return C_COMMENT;
+    }
+
+    public void setC_COMMENT (String C_COMMENT)
+    {
+        this.C_COMMENT = C_COMMENT;
+    }
+
+    public String getC_MKTSEGMENT ()
+    {
+        return C_MKTSEGMENT;
+    }
+
+    public void setC_MKTSEGMENT (String C_MKTSEGMENT)
+    {
+        this.C_MKTSEGMENT = C_MKTSEGMENT;
+    }
+
+    public String getC_CUSTKEY ()
+    {
+        return C_CUSTKEY;
+    }
+
+    public void setC_CUSTKEY (String C_CUSTKEY)
+    {
+        this.C_CUSTKEY = C_CUSTKEY;
+    }
+
+    @Override
+    public String toString()
+    {
+        return "ClassPojo [C_NATIONKEY = "+C_NATIONKEY+", C_ADDRESS = "+C_ADDRESS+", C_NAME = "+C_NAME+", C_PHONE = "+C_PHONE+", C_ACCTBAL = "+C_ACCTBAL+", C_COMMENT = "+C_COMMENT+", C_MKTSEGMENT = "+C_MKTSEGMENT+", C_CUSTKEY = "+C_CUSTKEY+"]";
+    }
+}
