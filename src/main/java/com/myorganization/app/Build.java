e75148850d4041284ff323dbda09dbf78f854a44
JAVASPRING AUTOWIRE GURU <57644059+JAVABEANSPRINGAUTOWIREGURU@users.noreply.github.com>
2019-11-12 00:24:10 +0000

    Create Build.java

---
+++ src/main/java/com/myorganization/app/Build.java
@@ -0,0 +1,23 @@
+                
+public class Build
+{
+    private Plugins plugins;
+
+    public Plugins getPlugins ()
+    {
+        return plugins;
+    }
+
+    public void setPlugins (Plugins plugins)
+    {
+        this.plugins = plugins;
+    }
+
+    @Override
+    public String toString()
+    {
+        return "ClassPojo [plugins = "+plugins+"]";
+    }
+}
+			
+	
