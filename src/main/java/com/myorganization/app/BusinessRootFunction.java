83df2eba18f9fb111322fa3292901e7e62f4051a
JAVASPRING AUTOWIRE GURU <57644059+JAVABEANSPRINGAUTOWIREGURU@users.noreply.github.com>
2019-11-12 00:26:10 +0000

    Create BusinessRootFunction.java

---
+++ src/main/java/com/myorganization/app/BusinessRootFunction.java
@@ -0,0 +1,81 @@
+        
+public class BusinessRootFunction
+{
+    private String likely;
+
+    private String image;
+
+    private String wide;
+
+    private String nearer;
+
+    private String bow;
+
+    private Shaking shaking;
+
+    public String getLikely ()
+    {
+        return likely;
+    }
+
+    public void setLikely (String likely)
+    {
+        this.likely = likely;
+    }
+
+    public String getImage ()
+    {
+        return image;
+    }
+
+    public void setImage (String image)
+    {
+        this.image = image;
+    }
+
+    public String getWide ()
+    {
+        return wide;
+    }
+
+    public void setWide (String wide)
+    {
+        this.wide = wide;
+    }
+
+    public String getNearer ()
+    {
+        return nearer;
+    }
+
+    public void setNearer (String nearer)
+    {
+        this.nearer = nearer;
+    }
+
+    public String getBow ()
+    {
+        return bow;
+    }
+
+    public void setBow (String bow)
+    {
+        this.bow = bow;
+    }
+
+    public Shaking getShaking ()
+    {
+        return shaking;
+    }
+
+    public void setShaking (Shaking shaking)
+    {
+        this.shaking = shaking;
+    }
+
+    @Override
+    public String toString()
+    {
+        return "ClassPojo [likely = "+likely+", image = "+image+", wide = "+wide+", nearer = "+nearer+", bow = "+bow+", shaking = "+shaking+"]";
+    }
+}
