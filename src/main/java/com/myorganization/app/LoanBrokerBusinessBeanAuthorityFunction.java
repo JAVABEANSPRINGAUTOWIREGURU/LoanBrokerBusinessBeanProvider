850ac8dd89c52a0a4ba0320a019812db7a2f8cfb
JAVASPRING AUTOWIRE GURU <57644059+JAVABEANSPRINGAUTOWIREGURU@users.noreply.github.com>
2019-11-12 00:25:47 +0000

    Create LoanBrokerBusinessBeanAuthorityFunction.java

---
+++ src/main/java/com/myorganization/app/LoanBrokerBusinessBeanAuthorityFunction.java
@@ -0,0 +1,22 @@
+public class LoanBrokerBusinessBeanAuthorityFunction
+{
+    private Root root;
+
+    public Root getRoot ()
+    {
+        return root;
+    }
+
+    public void setRoot (Root root)
+    {
+        this.root = root;
+    }
+
+    @Override
+    public String toString()
+    {
+        return "ClassPojo [root = "+root+"]";
+    }
+}
+			
+		
