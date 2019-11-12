5d8e38680819adb8811edf41aecc3ceb0915d3db
JAVASPRING AUTOWIRE GURU <57644059+JAVABEANSPRINGAUTOWIREGURU@users.noreply.github.com>
2019-11-12 00:26:37 +0000

    Create BusinessDiameterServiceAdapter.java

---
+++ src/main/java/com/myorganization/app/BusinessDiameterServiceAdapter.java
@@ -0,0 +1,81 @@
+       
+public class BusinessDiameterServiceAdapter
+{
+    private String correctly;
+
+    private String condition;
+
+    private String egg;
+
+    private String duty;
+
+    private String stranger;
+
+    private String sit;
+
+    public String getCorrectly ()
+    {
+        return correctly;
+    }
+
+    public void setCorrectly (String correctly)
+    {
+        this.correctly = correctly;
+    }
+
+    public String getCondition ()
+    {
+        return condition;
+    }
+
+    public void setCondition (String condition)
+    {
+        this.condition = condition;
+    }
+
+    public String getEgg ()
+    {
+        return egg;
+    }
+
+    public void setEgg (String egg)
+    {
+        this.egg = egg;
+    }
+
+    public String getDuty ()
+    {
+        return duty;
+    }
+
+    public void setDuty (String duty)
+    {
+        this.duty = duty;
+    }
+
+    public String getStranger ()
+    {
+        return stranger;
+    }
+
+    public void setStranger (String stranger)
+    {
+        this.stranger = stranger;
+    }
+
+    public String getSit ()
+    {
+        return sit;
+    }
+
+    public void setSit (String sit)
+    {
+        this.sit = sit;
+    }
+
+    @Override
+    public String toString()
+    {
+        return "ClassPojo [correctly = "+correctly+", condition = "+condition+", egg = "+egg+", duty = "+duty+", stranger = "+stranger+", sit = "+sit+"]";
+    }
+}
